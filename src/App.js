4895cac73cb216cee2b1f52b7f4e60f7b8cedca8
Allison Homem <mrs.allisonhomem@outlook.com>
2021-08-11 11:46:46 -0500

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {  useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [ apodData, setApodData ] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=xQXySdxom5h6szZtFdwgAqy78wLBXaylm2DpxbDm')
+         .then(res => {
+           setApodData(res.data);
+         })
+         .catch(err => {
+           console.error('failed to get data')
+         })
+  }, []);
+
+  console.log(apodData);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <Header />
+     <Body />
+     <p> This is working </p>
     </div>
   );
 }
